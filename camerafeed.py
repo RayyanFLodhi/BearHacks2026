b55d2803898623a6167405b2618b9060258569d7
RayyanFLodhi <lodhi.rayyan@gmail.com>
2026-04-25 13:44:57 -0400

    Drone Feed File

---
+++ camerafeed.py
@@ -0,0 +1,18 @@
+import cv2
+
+cap = cv2.VideoCapture(1)  # OBS virtual camera
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    cv2.imshow("Drone Feed", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
